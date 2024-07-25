32afb96552fc85ce328a8a9a640c17f6f15bf071
chebrolusrikanth <ch.srikanth0809@gmail.com>
2024-07-25 09:44:38 +0530

    project completed

--- pdf_chatbot/chatbot/tests.py
+++ pdf_chatbot/chatbot/tests.py
@@ -12,7 +12,6 @@ class PDFDocumentTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_query_pdf(self):
-        # Upload a test PDF
         url = reverse('pdfdocument-list')
         with open('test.pdf', 'rb') as pdf_file:
             response = self.client.post(url, {'file': pdf_file}, format='multipart')
@@ -20,7 +19,7 @@ class PDFDocumentTests(APITestCase):
 
         pdf_id = response.data['id']
 
-        # Query the uploaded PDF
+
         query_url = reverse('pdfdocument-query', args=[pdf_id])
         response = self.client.post(query_url, {'query': 'What is the content?'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
